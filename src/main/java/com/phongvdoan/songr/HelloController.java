15629ed295bea4dfb193df1538ccb18d06b488d8
Phong <phongdoan.dev@gmail.com>
2020-01-21 00:17:43 -0800

    capitalize feature completed

--- src/main/java/com/phongvdoan/songr/HelloController.java
+++ src/main/java/com/phongvdoan/songr/HelloController.java
@@ -1,13 +1,23 @@
 package com.phongvdoan.songr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloController {
 
-    @GetMapping('/hello')
+    @GetMapping("/hello")
     public String helloWorld(){
         return "hello";
+    }
+
+    @GetMapping("/capitalize/{word}")
+    public String captializeString(@PathVariable String word, Model m) {
+        String wordsInAllCaps = word.toUpperCase();
+        System.out.println(wordsInAllCaps);
+        m.addAttribute("capitalizedWord", wordsInAllCaps);
+        return "capitalize";
     }
 }
