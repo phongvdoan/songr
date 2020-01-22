2761123c4a35975ef4646afa9e0131d613d17e61
Phong <phongdoan.dev@gmail.com>
2020-01-22 13:58:46 -0800

    update almost completed

--- src/main/java/com/phongvdoan/songr/HomeController.java
+++ src/main/java/com/phongvdoan/songr/HomeController.java
@@ -3,10 +3,7 @@ package com.phongvdoan.songr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -54,6 +51,14 @@ public class HomeController {
     public RedirectView deleteAlbums(Long id){
         System.out.println("id = " + id);
         albumRepository.deleteById(id);
+        return new RedirectView("/albums");
+    }
+
+    @PutMapping("/albums")
+    public RedirectView updateAlbums(Long id){
+        Album album= albumRepository.getOne(id);
+        album.setArtist("BBBBAAAAMMMM");
+        albumRepository.save(album);
         return new RedirectView("/albums");
     }
 
